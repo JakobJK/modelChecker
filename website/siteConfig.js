06b2f6855be4fdba990793555a7492495c305779
JakobJK <jakobjk@gmail.com>
2019-09-17 11:50:34 -0400

    base url changes

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'modelChecker', // Title for your website.
   tagline: 'Unopiniated modelChecker for Autodesk Maya',
-  url: 'http://JakobJK.github.io/', // Your website URL
+  url: 'http://JakobJK.github.io', // Your website URL
   baseUrl: '/modelChecker/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
