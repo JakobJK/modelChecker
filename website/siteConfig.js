6fa5670ab1f79415d316a6a936cfd3ae2a95aeb6
JakobJK <jakobjk@gmail.com>
2019-09-17 11:37:49 -0400

    baseConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'modelChecker', // Title for your website.
   tagline: 'Unopiniated modelChecker for Autodesk Maya',
-  url: 'http://JakobJK.github.io/modelChecker', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'http://JakobJK.github.io/', // Your website URL
+  baseUrl: '/modelChecker/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
